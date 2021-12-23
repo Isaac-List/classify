e52d226293b6f31ab88c4b5d15d1adca5c85edf1
isaac-list <listis01@luther.edu>
2021-12-22 22:32:07 -0600

    transition module to use node-fetch

--- index.ts
+++ index.ts
@@ -4,14 +4,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const request = require('request');
+const node_fetch = require('node-fetch');
 const xml2js = require('xml2js').parseString;
+
 
 const isbn_ep = "http://classify.oclc.org/classify2/Classify?summary=true&isbn=";
 const owi_ep = "http://classify.oclc.org/classify2/Classify?summary=true&owi=";
 const title_ep = "http://classify.oclc.org/classify2/Classify?summary=true&title=";
 
 type callback = (response: any) => void;
+
 type response = {
   status: string;
   owi: string;
@@ -21,74 +23,53 @@ type response = {
   congress: string;
 };
 
-function getRequest(identifier: string, endpoint: string, callback: callback) {
+async function getRequest(identifier: string, endpoint: string, callback: callback) {
   let combined_endpoint = "";
 
   if (typeof(identifier) == "string") {
-  	combined_endpoint = endpoint + identifier;
+    combined_endpoint = endpoint + identifier;
   }
-  
+
   else if(Array.isArray(identifier)) {
-  	combined_endpoint = endpoint + identifier[0] + "&author=" + identifier[1];
+    combined_endpoint = endpoint + identifier[0] + "&author=" + identifier[1];
   }
-  
-  request({
-    url: combined_endpoint,
-    json: true,
-    headers: {
-      'User-Agent': 'npm-classify2'
-    }
-  },
-  function (error: any, response: any, body: any) {
-    if (error) {
-      callback(error);
-    }
 
-    /** @param result is json of xml body */
-    xml2js(body, function (err: any, result: any) {
-      let code = result.classify.response[0]["$"].code;
-      if (code == 4) {
-        let owi = result.classify.works[0].work[0]["$"].owi;
-        getRequest(owi, owi_ep, callback);
-      }
-      
-      else {
-        result = result.classify;
+  const response = await node_fetch(combined_endpoint);
+  const xml_body = await response.text();
 
-        try {
-          let response = {
-            status: result.response[0]['$'].code,
-            owi: result.work[0]["$"].owi,
-            author: result.work[0]["$"].author,
-            title: result.work[0]["$"].title,
-            dewey: result.recommendations[0].ddc[0].mostPopular[0]['$'].sfa,
-            congress: result.recommendations[0].lcc[0].mostPopular[0]['$'].sfa
-          }
-          callback(response)
-        } catch (e) {
-          console.log("Encountered an Error:", e);
+  xml2js(xml_body, function (err: any, result: any) {
+    let code = result.classify.response[0]["$"].code;
+
+    if (code == 4) {
+      let owi = result.classify.works[0].work[0]["$"].owi;
+      getRequest(owi, owi_ep, callback);
+    }
+    
+    else {
+      result = result.classify;
+
+      try {
+        let response = {
+          status: result.response[0]['$'].code,
+          owi: result.work[0]["$"].owi,
+          author: result.work[0]["$"].author,
+          title: result.work[0]["$"].title,
+          dewey: result.recommendations[0].ddc[0].mostPopular[0]['$'].sfa,
+          congress: result.recommendations[0].lcc[0].mostPopular[0]['$'].sfa
         }
+        callback(response)
+      } catch (e) {
+        console.log("Encountered an Error:", e);
       }
-    })
-  }
-  )
+    }
+  });
 }
 
-exports.classify = function (identifier: string, type: string, callback: callback) {
+exports.classify = (identifier: string, type: string, callback: callback) => {
   if (type == "isbn") {
     getRequest(identifier, isbn_ep, (data: any) => {
       callback(data);
     });
-  }
-
-  else if (type == "title-author") {
-  	getRequest(identifier, title_ep, (data: any) => {
-  	  callback(data);	
-  	});
-  }
-
-  else {
-  	callback("please provide a supported type: isbn or title-author");
   }
 }
 
