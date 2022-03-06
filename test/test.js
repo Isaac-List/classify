7aef99998b8682cf6599efa091c0f4284c510153
isaac-list <listis01@luther.edu>
2022-03-06 16:38:05 -0600

    implement preliminary testing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,24 @@
+const assert = require("assert");
+const classify = require("../index.js");
+
+describe('API Request Handler', function() {
+  it("Should work with ISBN's", function(done) {
+    classify.classify("isbn", ["9780380807345"], function (data) {
+	    assert.equal(data.title, "Coraline");
+	    assert.equal(data.congress, "PZ7.G1273");
+      done();
+	  });
+  });
+
+  it("Should return the proper error for No Info", function (done) {
+    classify.classify("isbn", ["9780380807343"], function (data) {
+      assert.equal(data, "No Information Found");
+      done();
+    });
+  });
+});
+
+/*
 var api = require('../index')
 var should = require('should')
 
@@ -24,3 +45,4 @@ describe('The API receiver', function () {
     })
   })
 })
+*/
