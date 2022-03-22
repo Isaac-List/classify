f0c3c150610c877683f6fd41c578b2280cbd04c8
isaac-list <listis01@luther.edu>
2022-03-22 13:45:36 -0500

    version bump and tests for multi-result response

--- test/test.js
+++ test/test.js
@@ -12,9 +12,11 @@ describe('API Request Handler', function() {
   });
 
   it("Should work with Title", function(done) {
-    this.timeout(3500);
-    classify.classify("title-author", ["Coraline", ""], function (data) {
-      assert.equal(data.title, "Coraline");
+    this.timeout(5000);
+    classify.classify("title-author", ["Paper towns", ""], function(data) {
+      assert.equal(data[0].code, '202483793');
+      assert.equal(data[4].code, '15560054');
+      assert.equal(data[1].format, "Book");
       done();
     });
   });
@@ -22,18 +24,12 @@ describe('API Request Handler', function() {
   it("Should work with Title and Author", function(done) {
     this.timeout(3500);
     classify.classify("title-author", ["Coraline", "Neil Gaiman"], function (data) {
-      assert.equal(data.title, "Coraline");
+      assert.equal(data[0].title, 'Coraline');
+      assert.equal(data[4].code, '862301469');
+      assert.equal(data[1].format, "Book");
       done();
     });
   });
-
-  // it("Should work with wi code", function(done) {
-  //   this.timeout(10000);
-  //   classify.classify("wi", ["49261060"], function (data) {
-	//     assert.equal(data.congress, "PZ7.G1273");
-  //     done();
-  //   });
-  // });
 
   it("Should return the proper error for No Info", function (done) {
     this.timeout(3500);
